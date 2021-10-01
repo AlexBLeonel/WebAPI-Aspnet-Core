76dd9a1bff25b30e9f6c0782c44c56b74430d326
AlexBLeonel <alex.leonel026@gmail.com>
2021-10-01 11:27:02 -0300

    Início de projeto  WebAPI.

---
+++ Alura.WebAPI.WebApp/Api/LivrosController.cs
@@ -0,0 +1,34 @@
+﻿using Alura.ListaLeitura.Modelos;
+using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.WebAPI.WebApp.Api
+{
+    public class LivrosController : Controller
+    {
+        private readonly IRepository<Livro> _repository;
+
+        public LivrosController(IRepository<Livro> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Recuperar(int id)
+        {
+            var model = _repository.Find(id); 
+
+            if(model == null)
+            {
+                return NotFound();
+            } else
+            {
+                return Json(model.ToModel());
+            }
+        }
+    }
+}
